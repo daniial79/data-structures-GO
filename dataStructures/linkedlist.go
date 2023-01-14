1ff87440a765c9660ee9cb9ab6fb60503a887a61
daniial79 <dpezeshpoor@yahoo.com>
2023-01-14 16:30:38 +0330

    ds/LinkedList/Append append method is implemented

--- dataStructures/linkedlist.go
+++ dataStructures/linkedlist.go
@@ -1,5 +1,7 @@
 package dataStructure
 
+import "fmt"
+
 type Node struct {
 	Value int
 	Next  *Node
@@ -17,4 +19,35 @@ func GenLinkedList() *LinkedList {
 		Tail:   nil,
 		Length: 0,
 	}
+}
+
+func (ll LinkedList) PrintList() {
+	if ll.Length == 0 {
+		fmt.Println("LinkedList is empty!")
+	} else {
+		temp := ll.Head
+
+		for temp.Next != nil {
+			fmt.Printf("%d => ", temp.Value)
+			temp = temp.Next
+		}
+		fmt.Println()
+	}
+
+}
+
+func (ll *LinkedList) Append(val int) {
+	node := Node{
+		Value: val,
+		Next:  nil,
+	}
+	if ll.Length == 0 {
+		ll.Head = &node
+		ll.Tail = &node
+	} else {
+		ll.Tail.Next = &node
+		ll.Tail = &node
+	}
+
+	ll.Length++
 }
--- main.go
+++ main.go
@@ -1,1 +1,15 @@
 package main
+
+import dataStructure "github.com/daniial79/data-structures-GO/dataStructures"
+
+func main() {
+	mll := dataStructure.GenLinkedList()
+
+	mll.PrintList()
+
+	for i := 1; i <= 10; i++ {
+		mll.Append(i)
+	}
+
+	mll.PrintList()
+}
