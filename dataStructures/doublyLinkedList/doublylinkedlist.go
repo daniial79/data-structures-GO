109d00278fc6049da013eb2bb5a71220e7484f6b
daniial79 <dpezeshpoor@yahoo.com>
2023-01-15 20:25:16 +0330

    feat: Append and PrintList methods of doubly linked list are implemented

--- dataStructures/doublyLinkedList/doublylinkedlist.go
+++ dataStructures/doublyLinkedList/doublylinkedlist.go
@@ -1,5 +1,9 @@
 package dll
 
+import (
+	"fmt"
+)
+
 type Node struct {
 	Value int
 	Next  *Node
@@ -14,4 +18,37 @@ type DoublyLinkedList struct {
 
 func GenDoublyLinkedList() *DoublyLinkedList {
 	return &DoublyLinkedList{}
+}
+
+func (dll *DoublyLinkedList) PrintList() {
+	if dll.Length == 0 {
+		fmt.Println("List is empty!")
+
+	} else if dll.Length == 1 {
+		fmt.Printf("NULL <=> %d <=> NULL\n", dll.Head.Value)
+	} else {
+		temp := dll.Head
+		fmt.Print("NULL <=> ")
+		for temp.Next != nil {
+			fmt.Printf("%d <=> ", temp.Value)
+			temp = temp.Next
+			if temp.Next == nil {
+				fmt.Printf("%d <=> NULL\n", temp.Value)
+			}
+		}
+	}
+}
+
+func (dll *DoublyLinkedList) Append(val int) {
+	node := Node{Value: val}
+	if dll.Length == 0 {
+		dll.Head = &node
+		dll.Tail = &node
+	} else {
+		node.Prev = dll.Tail
+		dll.Tail.Next = &node
+		dll.Tail = &node
+	}
+
+	dll.Length++
 }
--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
-	"fmt"
-
 	dll "github.com/daniial79/data-structures-GO/dataStructures/doublyLinkedList"
 )
 
 func main() {
 	mdll := dll.GenDoublyLinkedList()
 
-	fmt.Println(mdll)
+	// mdll.PrintList()
+
+	mdll.Append(1)
+	mdll.Append(2)
+	mdll.Append(3)
+	mdll.Append(4)
+	mdll.Append(5)
+
+	mdll.PrintList()
 }
