d5c78a0e0c2403a16bb63601bf1d5a2e90f7e65f
daniial79 <dpezeshpoor@yahoo.com>
2023-01-16 17:52:41 +0330

    feat: pop method of stack is implemented

--- dataStructures/stack/stack.go
+++ dataStructures/stack/stack.go
@@ -1,6 +1,9 @@
 package stack
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type node struct {
 	Value int
@@ -17,14 +20,18 @@ func GenStack() *Stack {
 }
 
 func (s *Stack) PrintStack() {
-	temp := s.Top
-	for temp.Rear != nil {
-		fmt.Printf(" %d\n", temp.Value)
-		fmt.Print("||\n")
-		fmt.Print("\\/\n")
-		temp = temp.Rear
+	if s.Height == 0 {
+		fmt.Println("stack is empty")
+	} else {
+		temp := s.Top
+		for temp.Rear != nil {
+			fmt.Printf(" %d\n", temp.Value)
+			fmt.Print("||\n")
+			fmt.Print("\\/\n")
+			temp = temp.Rear
+		}
+		fmt.Println("NULL")
 	}
-	fmt.Println("NULL")
 }
 
 func (s *Stack) Push(val int) {
@@ -36,4 +43,19 @@ func (s *Stack) Push(val int) {
 		s.Top = &node
 	}
 	s.Height++
+}
+
+func (s *Stack) Pop() (node *node, err error) {
+
+	if s.Height == 0 {
+		err = errors.New("stack is empty!")
+		node = nil
+	} else {
+		err = nil
+		node = s.Top
+		s.Top = s.Top.Rear
+		node.Rear = nil
+	}
+
+	return node, err
 }
--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"fmt"
+
 	stack "github.com/daniial79/data-structures-GO/dataStructures/stack"
 )
 
 func main() {
 	ms := stack.GenStack()
+
+	node, err := ms.Pop()
+	fmt.Println(err)
+	_ = node
 
 	for i := 0; i <= 9; i++ {
 		ms.Push(i)
