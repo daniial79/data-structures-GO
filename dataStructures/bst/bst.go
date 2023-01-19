2a2f74e1ca480a49d7e35ca18dba11b03b4013b4
daniial79 <dpezeshpoor@yahoo.com>
2023-01-19 19:39:34 +0330

    feat: insert method of bst is implemented

--- dataStructures/bst/bst.go
+++ dataStructures/bst/bst.go
@@ -13,3 +13,32 @@ type Bst struct {
 func GenBst() *Bst {
 	return &Bst{}
 }
+
+func (b *Bst) Insert(val int) bool {
+	node := node{value: val}
+	if b.Root == nil {
+		b.Root = &node
+		return true
+	}
+	temp := b.Root
+
+	for {
+		if temp.value == node.value {
+			break
+		} else if temp.value > node.value {
+			if temp.left == nil {
+				temp.left = &node
+				break
+			}
+			temp = temp.left
+		} else if temp.value < node.value {
+			if temp.right == nil {
+				temp.right = &node
+				break
+			}
+			temp = temp.right
+		}
+	}
+
+	return true
+}
