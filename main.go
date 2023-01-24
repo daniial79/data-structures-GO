d36719dcb132395d4fa7681c3b8f6b2d02ee2044
daniial79 <dpezeshpoor@yahoo.com>
2023-01-24 19:06:01 +0330

    feat: insertion sort algorithm is implemented

--- main.go
+++ main.go
@@ -1,33 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/daniial79/data-structures-GO/dataStructures/hashTable"
-)
-
 func main() {
-	mht := hashTable.GenHashTable()
-
-	fmt.Printf("%+v\n", mht)
-
-	mht.Set(hashTable.Pair{
-		Key:   "nails",
-		Value: 1000,
-	})
-
-	mht.Set(hashTable.Pair{
-		Key:   "bolts",
-		Value: 750,
-	})
-
-	mht.Set(hashTable.Pair{
-		Key:   "pins",
-		Value: 500,
-	})
-
-	fmt.Printf("%+v\n", mht)
-
-	fmt.Printf("%#v\n", mht.Keys())
 
 }
